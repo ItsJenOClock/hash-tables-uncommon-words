569b57d716bb35e7b3561879bdbec829e8867b11
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-28 14:32:30 -0400

    implementation of original.py

--- activity/original.py
+++ activity/original.py
@@ -1,2 +1,14 @@
 def uncommon_from_sentences(s1, s2):
-    pass
+    uncommon_words = []
+    word_dict = {}
+
+    for word in s1.split():
+        word_dict[word] = word_dict.get(word, 0) + 1
+    for word in s2.split():
+        word_dict[word] = word_dict.get(word, 0) + 1
+    
+    for word, count in word_dict.items():
+        if count == 1:
+            uncommon_words.append(word)
+    
+    return uncommon_words
